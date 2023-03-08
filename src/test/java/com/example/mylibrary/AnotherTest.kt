414a0a40d28e9aa10804931c706c26f64f28bc14
Inaki Villar <inaki.seri@gmail.com>
2023-03-08 13:03:06 -0800

    initial import

---
+++ src/test/java/com/example/mylibrary/AnotherTest.kt
@@ -0,0 +1,15 @@
+package com.example.mylibrary
+
+class AnotherTest {
+
+    @Override
+    fun test1(){
+        assert(true)
+    }
+
+    @Override
+    fun test2(){
+        assert(true)
+    }
+
+}
